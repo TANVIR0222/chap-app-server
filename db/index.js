3ba6730cfd78f51da444df07d44afde8122e825c
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-03-26 22:25:43 +0600

    connect database

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(process.env.BD_URL)
-        console.log(`MongoDB Connected !! DB Host ${connectionInstance.connection.host}`);
+        console.log(`MongoDB Connected Database  !! DB Host ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log('Mongodb connection Failed : ', error);
         process.exit(1)
